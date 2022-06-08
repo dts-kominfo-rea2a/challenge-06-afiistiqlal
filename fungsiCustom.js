f34d2ca8e496e205793b7a01c802c112e592da92
afiistiqlal <afiistiqlal@gmail.com>
2022-06-08 14:59:14 +0700

    [New] bacaData

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,40 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const fnCallback = (err, data) => {
+  if (err) {
+    return console.log(err);
+  }
+  return data;
+}
+
+const files = [file1, file2, file3];
+const bacaData = (fnCallback) => {
+  let dataFile = "";
+  const hasil = [];
+  files.forEach(element => {
+    fs.readFile(element, 'utf8', (err, data) => {
+      if (err) {
+        return(fnCallback("Error! Something goes wrong!", null));
+      }
+
+      data = JSON.parse(data);
+      if (element == file1) {
+        dataFile = data.message;
+      }
+      if (element == file2) {
+        dataFile = data[0].message;
+      }
+      if (element == file3) {
+        dataFile = data[0].data.message;
+      }
+      const arrHasil = dataFile.split(" ");
+      hasil.push(arrHasil[1]);
+      return hasil;
+    });
+  });
+  fnCallback(undefined, hasil);
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
